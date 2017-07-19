c06f988fa3fb4a253acc9f6de92975c5615d4674
F.Boyang <flboy@outlook.com>
2017-07-19 18:33:19 +0200

    chooseDate.xhtml

--- frog-travel/src/main/java/org/camunda/bpm/frogtravel/service/ServiceSelectServiceBean.java
+++ frog-travel/src/main/java/org/camunda/bpm/frogtravel/service/ServiceSelectServiceBean.java
@@ -2,6 +2,8 @@ package org.camunda.bpm.frogtravel.service;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.frogtravel.persistence.Customer;
+import org.camunda.bpm.frogtravel.persistence.PaymentInfo;
+import org.camunda.bpm.frogtravel.persistence.ServiceSelection;
 
 import javax.ejb.Stateless;
 import javax.inject.Named;
@@ -19,12 +21,22 @@ public class ServiceSelectServiceBean {
 	private EntityManager entityManager;
 	
 	public void persistCustomer(DelegateExecution delegateExecution) {
-		Customer customerEntity = new Customer();
+		ServiceSelection serviceSelectEntity = new ServiceSelection();
 		
 		// Get all process variables
 	    Map<String, Object> variables = delegateExecution.getVariables();
 	    
-		
+	    // Set order attributes
+	    serviceSelectEntity.setArriveTime((Date) variables.get("arriveDate"));
+	    serviceSelectEntity.setReturnTime((Date) variables.get("returnDate"));
+	    serviceSelectEntity.setDestination((String) variables.get("destination"));
+
+	    /*
+	      Persist order instance and flush. After the flush the
+	      id of the order instance is set.
+	    */
+	    entityManager.persist(serviceSelectEntity);
+	    entityManager.flush();
 	
 	}
 
